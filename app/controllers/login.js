1d23674461545a6dffa315e4964830a865a63182
James-Mac <james@aehr.org>
2014-07-03 13:19:12 -0600

    Start on input validation

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 
 var LoginController = Ember.Controller.extend({
 
+  // Input Validation
+  idIsValid: function () {
+    if(!this.get('cid')) {
+      return false;
+    }
+    // Make sure the card id is 9 characters long
+    return (this.get('cid').length === 9);
+  }.property('cid')
+  
 });
 
 export default LoginController;
