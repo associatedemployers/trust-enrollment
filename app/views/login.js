fd30a8b5576ed493aafa3b9249bf039793e8ab17
James-Mac <james@aehr.org>
2014-07-07 14:10:00 -0600

    Add template property just in case

--- app/views/login.js
+++ app/views/login.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import renderTooltipsMixin from '../mixins/render-tooltips';
 
 var LoginView = Ember.View.extend(renderTooltipsMixin, {
+  templateName: 'login',
   classNames: [ 'card', 'login-card', 'center-vertical' ]
 });
 
