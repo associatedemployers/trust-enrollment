2ba7649506445ba3eea90d158b909aebe8de84d5
James-Mac <james@aehr.org>
2014-07-07 11:30:24 -0600

    Finish up login inline validation

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -13,14 +13,34 @@ var LoginController = Ember.Controller.extend({
     return (cid.length === 9 && parseFloat(cid.substr(0, 3)) === 943 && !isNaN(cid));
   }.property('cid'),
 
+  ssnIsValid: function () {
+    var ssn = this.get('ssn');
+    if(!ssn) {
+      return false;
+    }
+    // Make sure the ssn is 9 characters long and a number
+    return (ssn.length === 9 && !isNaN(ssn));
+  }.property('ssn'),
+
   loginIsValid: function () {
+    // If this is a new enrollment, we return the ssn validity. If not, we return ssn & cid validity.
+    return (this.get('newEnrollment')) ? this.get('ssnIsValid') : this.get('idIsValid') && this.get('ssnIsValid');
+  }.property('ssnIsValid', 'idIsValid'),
 
-  },
+  // Regex formatting
+  cid_formatted: function () {
+    if(!this.get('cid')) {
+      return;
+    }
+    return this.get('cid').replace(/(\d{3})(\d{2})(\d{4})/, '$1-$2-$3');
+  }.property('cid'),
 
-  cidDidChange: function () {
-    var cid = this.get('cid');
-    this.set('cid_formatted', cid.replace(/(\d{3})(\d{2})(\d{4})/, '$1-$2-$3'));
-  }.observes('cid'),
+  ssn_formatted: function () {
+    if(!this.get('ssn')) {
+      return;
+    }
+    return this.get('ssn').replace(/(\d{3})(\d{2})(\d{4})/, '***-**-$3');
+  }.property('ssn'),
 
   actions: {
     // Implement a toggleProperty action handler to nerf a style depreciation message. Yeah, that's right, I said a STYLE DEPRECIATION.
