018df4020e9ef8383194b79b06b38980ba07f765
James-Mac <james@aehr.org>
2014-07-11 14:53:05 -0600

    Fix another failing test

--- tests/unit/controllers/header-test.js
+++ tests/unit/controllers/header-test.js
@@ -2,7 +2,7 @@ import { test, moduleFor } from 'ember-qunit';
 
 moduleFor('controller:header', 'HeaderController', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['controller:application']
 });
 
 // Replace this with your real tests.
