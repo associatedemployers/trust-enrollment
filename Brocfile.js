062379414fb150a2a525b06ebe8f2841cd5ade6b
James-Mac <james@aehr.org>
2014-07-15 07:52:12 -0600

    Add in momentjs dependency

--- Brocfile.js
+++ Brocfile.js
@@ -19,5 +19,6 @@ app.import('vendor/font-awesome/css/font-awesome.css');
 
 /* Vendor JS */
 app.import('vendor/bootstrap/dist/js/bootstrap.min.js');
+app.import('vendor/moment/moment.js');
 
 module.exports = app.toTree();
