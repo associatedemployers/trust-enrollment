9f139382618cb8055dc79c6490fbf13a48380771
James-Mac <james@aehr.org>
2014-08-05 12:59:52 -0600

    Responsive form menu

--- app/controllers/scroll-spy.js
+++ app/controllers/scroll-spy.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   needs: [ 'enrollment' ],
-  progress: Ember.computed.alias('parentController.progress')
+  progress: Ember.computed.alias('parentController.progress'),
+
+  actions: {
+    toggleProperty: function (prop) {
+      this.toggleProperty(prop);
+    }
+  }
 });
