7c7605c7c1425284e7f554920b54774598243abb
James-Mac <james@aehr.org>
2014-08-06 13:36:59 -0600

    Accept array for option removal

--- app/components/relationship-picker.js
+++ app/components/relationship-picker.js
@@ -5,17 +5,20 @@ export default Ember.Component.extend({
   staticRelationships: staticRelationships,
   
   relationships: function () {
-    var removeOptions = this.get('removeOptions');
+    var removeOptions = this.get('removeOptions'),
+        relarr = this.get('staticRelationships');
 
-    var removeOption = function (relationships, option) {
+    var removeOption = function (relationships, option, arr) {
       return relationships.filter(function (relationship) {
-        return option !== relationship;
+        return (arr) ? option.indexOf(relationship) < 0 : option !== relationship;
       });
     };
     
-    if(this.get('removeOptions')) {
+    if(removeOptions) {
       if(typeof removeOptions === 'string') {
-        return removeOption(this.get('staticRelationships'), removeOptions);
+        return removeOption(relarr, removeOptions);
+      } else if(typeof removeOptions === 'object' && removeOptions.length) {
+        return removeOption(relarr, removeOptions, true);
       }
     } else {
       return this.get('staticRelationships');
