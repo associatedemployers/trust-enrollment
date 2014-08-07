3d93261a494c8522f05569298ce499737786f54a
James-Mac <james@aehr.org>
2014-08-07 14:54:55 -0600

    Enrollment controller test

--- tests/unit/controllers/enrollment-test.js
+++ tests/unit/controllers/enrollment-test.js
@@ -1,12 +1,48 @@
+import Ember from 'ember';
 import { test, moduleFor } from 'ember-qunit';
 
 moduleFor('controller:enrollment', 'EnrollmentController', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
 // Replace this with your real tests.
 test('it exists', function() {
   var controller = this.subject();
   ok(controller);
+});
+
+test('it should validate & format values entered into @entries', function () {
+  expect(2);
+
+  var controller = this.subject();
+
+  Ember.run(function () {
+    controller.setProperties({
+      entries: [
+        {
+          _valName: 'firstName',
+          sectionIndex: 1,
+          format: function (v) {
+            return 'format' + v;
+          },
+          validity: {
+            _check: function (txt) {
+              return (txt === 'formattest');
+            },
+            description: "Test Value"
+          }
+        }
+      ],
+      model: Ember.Object.create({})
+    });
+
+    controller.set('content.firstName', 'test');
+  });
+
+  equal(controller.get('validity.firstName'), true, 'validity.firstName should be true when given test');
+
+  Ember.run(function () {
+    controller.set('content.firstName', 'notvalid');
+  });
+
+  equal(controller.get('validity.firstName'), false, 'validity.firstName should be false when given notvalid');
 });
